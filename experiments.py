5370d9526623728843452333f22618dfa842e405
Huang Feng <hf20051210@163.com>
2020-09-09 22:29:01 +0800

    Update experiments.py

--- experiments.py
+++ experiments.py
@@ -9,6 +9,60 @@ class Experiments(object):
         self.mir_dis_data = mir_dis_data
         self.model = Model(model_name)
         self.parameters = kwargs
+        
+    def CV_type(self):
+        k_folds = 5
+        association_matrix = self.mir_dis_data.type_tensor.sum(2)
+        index_matrix = np.array(np.where(association_matrix > 0))
+        pair_num = index_matrix.shape[1]
+        sample_num_per_fold = int(pair_num / k_folds)
+        np.random.seed(0)
+        np.random.shuffle(index_matrix.T)
+        metrics = 0
+        for k in range(k_folds):
+
+            train_tensor = np.array(self.mir_dis_data.type_tensor, copy=True)
+            if k != k_folds - 1:
+                test_index = tuple(index_matrix[:, k * sample_num_per_fold: (k + 1) * sample_num_per_fold])
+            else:
+                test_index = tuple(index_matrix[:, k * sample_num_per_fold:])
+
+            train_tensor[test_index] = 0
+            train_matrix = train_tensor.sum(2)
+            train_matrix[np.where(train_matrix > 0)] = 1
+
+            miRNA_func_similarity_matrix = np.mat(self.mir_dis_data.get_functional_sim(train_matrix))
+            predict_tensor = self.model()(train_tensor, self.mir_dis_data.dis_sim, miRNA_func_similarity_matrix,
+                                             r=self.parameters['r'], alpha=self.parameters['alpha'],
+                                             beta=self.parameters['beta'], lam=self.parameters['lam'],
+                                             tol=1e-6, max_iter=500)
+            TP = 0
+            recall = 0
+            real_sum = 0
+
+            sample_num_eval = np.array(test_index).shape[1]
+            for t in range(sample_num_eval):
+                predict_matrix = predict_tensor[test_index[0][t], test_index[1][t]]
+                predict_score = np.mat(predict_matrix.flatten())
+                real_matrix = association_tensor[test_index[0][t], test_index[1][t]]
+                real_score = np.mat(real_matrix.flatten())
+                positive_num = real_score.sum()
+                real_sum = real_sum + positive_num
+
+                sort_index = np.array(np.argsort(predict_score))[0]
+                predict_score[np.where(predict_score != 0)] = 0
+                predict_score[:, sort_index[-1:]] = 1
+
+                tp = predict_score * real_score.T
+
+                TP = TP + tp[0, 0]
+                recall = recall + tp[0, 0] / positive_num
+            avg_precision = TP / (1 * sample_num_eval)
+            mi_avg_recall = TP / real_sum
+            ma_avg_recall = recall / sample_num_eval
+            metrics = metrics + np.array([avg_precision, mi_avg_recall, ma_avg_recall])
+
+        return metrics / k_folds
 
     def CV_triplet(self):
         k_folds = 5
